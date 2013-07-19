1039113aec65929a3a84a76ad36ea9301e1003b0
kokubun <takashi_kokubun@tekunodo.jp>
2013-07-19 10:08:34 +0900

    Avoid creating duplicate musics

--- app/models/music.rb
+++ app/models/music.rb
@@ -4,4 +4,11 @@ class Music < ActiveRecord::Base
   validates :playtype, :presence => true
   validates :difficulty, :presence => true
   validates :notes, :presence => true
+
+  def self.create(args = {})
+    if Music.where(title: args[:title], playtype: args[:playtype], difficulty: args[:difficulty]).size == 0
+      @music = Music.new(title: args[:title], playtype: args[:playtype], difficulty: args[:difficulty], notes: args[:notes], level: args[:level])
+      @music.save
+    end
+  end
 end
--- spec/models/music_spec.rb
+++ spec/models/music_spec.rb
@@ -15,4 +15,12 @@ describe Music do
       invalid_music.should_not be_valid
     end
   end
+
+  describe ".create" do
+    it "should create a music, and should not create duplicate musics" do
+      Music.create(level: 12, title: "冥", difficulty: "A", playtype: "SP", notes: 2000)
+      Music.create(level: 12, title: "冥", difficulty: "A", playtype: "SP", notes: 2000)
+      Music.where(title: "冥", playtype: "SP", difficulty: "A").size.should == 1
+    end
+  end
 end
