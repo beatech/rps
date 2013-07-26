446f73d1652be2932034f3959e893792177277c2
kokubun <kokubun.t.aa@m.titech.ac.jp>
2013-07-27 02:35:47 +0900

    Add ranking column to users

--- app/assets/javascripts/user_sort.js
+++ app/assets/javascripts/user_sort.js
@@ -4,12 +4,12 @@ function scoreSort(position) {
       return parseFloat(scoreOfTr($(a), position)) > parseFloat(scoreOfTr($(b), position)) ? -1 : 1;
     })
   );
-
   $("#DP tbody").html(
     $("#DP tbody tr").sort(function(a, b) {
       return parseFloat(scoreOfTr($(a), position)) > parseFloat(scoreOfTr($(b), position)) ? -1 : 1;
     })
   );
+  refreshRanking();
 }
 
 function clearSort(position) {
@@ -18,12 +18,12 @@ function clearSort(position) {
       return parseFloat(clearOfTr($(a), position)) > parseFloat(clearOfTr($(b), position)) ? -1 : 1;
     })
   );
-
   $("#DP tbody").html(
     $("#DP tbody tr").sort(function(a, b) {
       return parseFloat(clearOfTr($(a), position)) > parseFloat(clearOfTr($(b), position)) ? -1 : 1;
     })
   );
+  refreshRanking();
 }
 
 function musicSort(position) {
@@ -32,12 +32,26 @@ function musicSort(position) {
       return musicOfTr($(a), position) > musicOfTr($(b), position) ? -1 : 1;
     })
   );
-
   $("#DP tbody").html(
     $("#DP tbody tr").sort(function(a, b) {
       return musicOfTr($(a), position) > musicOfTr($(b), position) ? -1 : 1;
     })
-  );
+  );  
+  refreshRanking();
+}
+
+function refreshRanking() {
+  var count = $("#SP tbody tr").length;
+  var i;
+
+  var spTr = $("#SP tbody tr").first();
+  var dpTr = $("#DP tbody tr").first();
+  for (i = 0; i < count; i++) {
+    spTr.children().eq(0).html((i + 1).toString());
+    dpTr.children().eq(0).html((i + 1).toString());
+    spTr = spTr.next();
+    dpTr = dpTr.next();
+  }
 }
 
 function clearOfTr(tr, position) {
