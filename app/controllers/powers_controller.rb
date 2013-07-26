942975fc315cd4de26774df997a18a2b5f11e259
kokubun <kokubun.t.aa@m.titech.ac.jp>
2013-07-26 10:00:48 +0900

    Fix lamp_num

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -91,7 +91,7 @@ class PowersController < ApplicationController
       fc_num += 1 if score[:clear] == "FC"
       exh_num += 1 if score[:clear] == "EXH"
       h_num += 1 if score[:clear] == "H"
-      lamp_num += 1
+      lamp_num += 1 if score[:clear] != "-"
       if score[:bp] != "-"
         bp_sum += score[:bp].to_i
         score_num += 1
@@ -127,9 +127,12 @@ class PowersController < ApplicationController
     end
 
     # For debug
-    # raise "LEVEL:"+level.to_s+"\n,IIDXID:"+iidxid+",\nSCORE_NUM:"+score_num.to_s+",\nLAMP_NUM:"+lamp_num.to_s+",\nFC_NUM:"+fc_num.to_s+
-    #   "\n,EXH_NUM:"+exh_num.to_s+"\n,H_NUM:"+h_num.to_s+"\n,FC_RATE:"+fc_rate.to_s+"\n,EXH_RATE:"+exh_rate.to_s+"\n,H_RATE:"+h_rate.to_s+
-    #   "\n,BP_AVE:"+bp_ave.to_s+"\n,BASE_POINT:"+base_point.to_s+"\n,K:"+k.to_s+"\n,CLEAR_POWER:"+("%.2f" % clear_power)
+    if level == 11
+      raise "LEVEL:"+level.to_s+"\n,IIDXID:"+iidxid+",\nSCORE_NUM:"+score_num.to_s+",\nLAMP_NUM:"+lamp_num.to_s+",\nFC_NUM:"+fc_num.to_s+
+      "\n,EXH_NUM:"+exh_num.to_s+"\n,H_NUM:"+h_num.to_s+"\n,FC_RATE:"+fc_rate.to_s+"\n,EXH_RATE:"+exh_rate.to_s+"\n,H_RATE:"+h_rate.to_s+
+      "\n,BP_AVE:"+bp_ave.to_s+"\n,BASE_POINT:"+base_point.to_s+"\n,K:"+k.to_s+"\n,CLEAR_POWER:"+("%.2f" % clear_power)
+    end
+
 
     "%.2f" % clear_power
   end
